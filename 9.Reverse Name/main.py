1d039459b6eee5ce51af1514c2e5ad1f1eaae1ce
Rahul Wadwani <rahulwadwani786@gmail.com>
2025-04-13 11:46:29 +0530

    Reversed name

--- 9.Reverse Name/main.py
+++ 9.Reverse Name/main.py
@@ -1,9 +1,14 @@
-"""
-main.py - Starter script
-"""
-
 def main():
-    print("This is the main script.")
+    print("Reverse Name Generator")
+    while True:
+        name = input("Enter a name: ")
+        reverse_name = name[::-1]
+        print(f"Your reversed name is:{reverse_name}")
+        print(f"In a parallel universe, they call you {reverse_name}")
+        query = input("Try another name? (y/n): ")
+        if query == 'n':
+            break
+        
 
 if __name__ == "__main__":
     main()
