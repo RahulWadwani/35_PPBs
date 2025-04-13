476e6288cbcde964e6bb94f0616e2ab9d8ca64f3
Rahul Wadwani <rahulwadwani786@gmail.com>
2025-04-13 11:47:31 +0530

    Randomized name generated

--- 8.Random Name Generator/main.py
+++ 8.Random Name Generator/main.py
@@ -1,9 +1,14 @@
-"""
-main.py - Starter script
-"""
+import random 
 
 def main():
-    print("This is the main script.")
+    print("Fanatsy Name Generator")
+    first_parts = ["sky","star","Moon","Sun","Fire","Ice"]
+    last_parts = ['rider','walker','hunter','seeker','dancer','keeper','singer']
+    count = int(input("How many names do you want? "))
+    for i in range(count):
+        first = random.choice(first_parts)
+        last = random.choice(last_parts)
+        print(f'{first}{last}')
 
 if __name__ == "__main__":
     main()
