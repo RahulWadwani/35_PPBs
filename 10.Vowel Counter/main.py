40f9022d4a8d021d5f6fcd91583e3bae23bf9fe6
Rahul Wadwani <rahulwadwani786@gmail.com>
2025-04-13 11:49:55 +0530

    counting vowels

--- 10.Vowel Counter/main.py
+++ 10.Vowel Counter/main.py
@@ -1,9 +1,16 @@
-"""
-main.py - Starter script
-"""
-
 def main():
-    print("This is the main script.")
+    print("Vowels Counter")
+    vowels = ['a','e','i','o','u']
+    while True:
+        sentence = input("Enter some text (or 'quit'):")
+        counter = 0
+        if  sentence.lower() == 'quit':
+            break
+        for letters in sentence.lower():
+            if letters in vowels:
+                counter  += 1
+        print(f"That text has {counter} vowels!")
+            
 
 if __name__ == "__main__":
     main()
