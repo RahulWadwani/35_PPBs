dacb6993d8074eeae365b0c2a9081db68ded60f7
Rahul Wadwani <rahulwadwani786@gmail.com>
2025-04-13 11:49:15 +0530

    scrambled words using random module

--- 6.Word Scrambler/main.py
+++ 6.Word Scrambler/main.py
@@ -1,9 +1,19 @@
-"""
-main.py - Starter script
-"""
+import random
 
 def main():
-    print("This is the main script.")
+    print("Word Scrambler")
+    while True:
+        word = input("Enter a word to scramble (or 'quit'): ").strip()
+        if word.lower() == 'quit':
+            print("Goodbye!")
+            break
+        if len(word.split(' ')) == 1:
+            #scrambled text and print the output 
+            letters = list(word)
+            random.shuffle(letters)
+            print(f'Scrambled: {"".join(letters)}')
+            
+        
 
 if __name__ == "__main__":
     main()
