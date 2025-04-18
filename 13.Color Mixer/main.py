56364c72993493bb13bff4cc3221a7abd2472edc
Rahul Wadwani <rahulwadwani786@gmail.com>
2025-04-18 21:17:55 +0530

    i am mixing colors

--- 13.Color Mixer/main.py
+++ 13.Color Mixer/main.py
@@ -1,9 +1,21 @@
-"""
-main.py - Starter script
-"""
-
-def main():
-    print("This is the main script.")
-
+def main(first,second):
+    print("Color Mixer")
+    colors = {
+        ("red","blue"):"purple",
+        ("red","yellow"):"orange",
+        ("blue","yellow"):"green",
+        ("blue","green"):"teal",
+        ("red","white"):"pink",
+        ("red","green"):"brown",
+    }
+    
+    while True:
+        if first.lower() == colors[0]
+        print(f"when you mix {first} and {second}, you get !")
+        mcolor = input("Mix more colors? (y/n): ")
+        if mcolor == 'n':
+            break
 if __name__ == "__main__":
-    main()
+    fcolor = input("Enter first color: ")
+    scolor = input("Enter second color: ")
+    main(fcolor,scolor)
