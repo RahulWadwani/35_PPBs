d93de03e7ece2b626f80021d0aff2c37d9808e7d
Rahul Wadwani <rahulwadwani786@gmail.com>
2025-04-18 21:19:30 +0530

    count it down

--- 16.Countdown Timer/main.py
+++ 16.Countdown Timer/main.py
@@ -1,9 +1,20 @@
-"""
-main.py - Starter script
-"""
+import time 
 
 def main():
-    print("This is the main script.")
+    print("=== COUNTDOWN TIMER ===")
+    print("Count down from your chosen seconds!")
+    while True:
+        timeis = int(input("Enter seconds to coundown from: "))
+        for i in range(timeis,0,-1):
+            print(f"{i} seconds remaining ...")
+            time.sleep(1)
+        print("Countdown complete!")
+        user = input("Start another countdown? (yes/no): ")
+        if user == 'no' or user == 'n':
+            print("Thanks for using the countdown timer!")
+            break
+    
+        
 
 if __name__ == "__main__":
     main()
